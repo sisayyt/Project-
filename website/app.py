faf6552a779c518008e9b32f732ffa41a4f1e762
Lori B <47092293+loribeth18@users.noreply.github.com>
2019-06-21 21:06:40 -0500

    Add files via upload

---
+++ website/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, jsonify
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'citydata'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/citydata'
+
+mongo = PyMongo(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/cityPop')
+def getCityPop():
+    collection = mongo.db.cityPopulation
+    cursor = collection.find({})
+    docs = []
+    for document in cursor:
+        del document['_id']
+        docs.append(document)
+    return jsonify(docs)
+
+@app.route('/cityPop2/<city>')
+def getCityPop2(city):
+    collection = mongo.db.cityPopulation
+    cursor = collection.find({})
+    docs = []
+    for document in cursor:
+        del document['_id']
+        if (document['city'] == city):
+            docs.append(document)
+    return jsonify(docs)
+
+@app.route('/cityCrime')
+def getCityCrime():
+    collection = mongo.db.cityCrime
+    cursor = collection.find({})
+    docs = []
+    for document in cursor:
+        del document['_id']
+        docs.append(document)
+    return jsonify(docs)
+
+@app.route('/cityCrime2/<city>')
+def getCityCrime2(city):
+    collection = mongo.db.cityCrime
+    cursor = collection.find({})
+    docs = []
+    for document in cursor:
+        del document['_id']
+        if (document['city'] == city):
+            docs.append(document)
+    return jsonify(docs)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ website/static/js/app.js
@@ -0,0 +1,78 @@
+function buildMetadata(city) {
+  //alert(sample);
+   
+  var url = `/cityPop2/${city}`; 
+  d3.json(url).then((data) =>{
+    //console.log(data);
+    var PANEL = d3.select("#city-sample");
+
+    PANEL.html("");
+
+    Object.entries(data[0]).forEach(([key,value]) =>{
+      PANEL.append("h6").text(`${key}: ${value}`);
+    })
+    
+  });
+}
+  
+
+function buildCharts(city) {
+
+  // @TODO: Use `d3.json` to fetch the sample data for the plots
+  var url = `/cityCrime2/${city}`; 
+  d3.json(url).then((data) =>{
+
+    // @TODO: Build a Chart using the sample data
+    const city = data[0].city;
+    const crimetype = data[0].type;
+    const year = ["2010","2011","2012","2013","2014","2015","2016"]
+    const yearValues = [data[0]["2010"],data[0]["2011"],data[0]["2012"],data[0]["2013"],data[0]["2014"],data[0]["2015"],data[0]["2016"]]
+
+    var bardata = [{
+      name: city,
+      x: year,
+      y: yearValues,
+      //data.map(row => row.["2016"]),
+      //text: data.map(row => row.type),
+      type: 'bar',
+      //hovertext: otu_labels.slice(0,10),
+      //hoverinfo: "hovertext",
+    }];
+
+    var barlayout = {
+      margin: {t:0,1:0}
+    };
+    Plotly.purge('bar');
+    Plotly.plot('bar', bardata, barlayout);
+  });
+
+ }
+
+function init() {
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  d3.json("/cityPop").then((dataPop) => {
+    dataPop.forEach((pop) => {
+      selector
+        .append("option")
+        .text(pop.city)
+        .property("value", pop.city);
+    });
+
+    // Use the first sample from the list to build the initial plots
+     const firstCity = dataPop[0];
+     buildCharts(firstCity.city);
+     buildMetadata(firstCity.city);
+  });
+}
+
+function optionChanged(newCity) {
+  // Fetch new data each time a new sample is selected
+  buildCharts(newCity);
+  buildMetadata(newCity);
+  //alert("You chose " + newCity + "! Wow!")
+}
+
+// Initialize the dashboard
+init();
