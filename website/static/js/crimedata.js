32400ca217b961a18b1d16b75683086932322350
Van86 <47127087+Van86@users.noreply.github.com>
2019-07-09 22:53:03 -0500

    Update crimedata.js

--- website/static/js/crimedata.js
+++ website/static/js/crimedata.js
@@ -1,53 +1,52 @@
-// Store our API endpoint inside queryUrl
-var queryUrl = "../static/data/cleanData.json"
-//var queryUrl = "static/data/zillow.json"
+//Creating map object
+var myMap = L.map("map", {
+      center: [
+        32.7763, -96.7969
+      ],
+      zoom: 10,
+  
+    });
 
-// Perform a GET request to the query URL
-d3.json(queryUrl).then((data) =>{
-  // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data);
-  //alert(data)
+// Adding tile layer to the map
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// Store API query variables
+var baseURL = "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?";
+var date = "$where=created_date between'2016-01-10T12:00:00' and '2017-01-01T14:00:00'";
+var complaint = "&complaint_type=Rodent";
+var limit = "&$limit=10000";
+
+// Assemble API query URL
+var url = "../static/data/crime.geojson"
+
+// Grab the data with d3
+d3.json(url, function(response) {
+
+  // Create a new marker cluster group
+  var markers = L.markerClusterGroup();
+
+  // Loop through data
+  for (var i = 0; i <  response && response.length; i++) {
+
+    // Set the data location property to a variable
+    var location = response[i].coordinates;
+
+    // Check for location property
+    if (location) {
+
+      // Add a new marker to the cluster group and bind a pop-up
+      markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
+        .bindPopup(response[i]["Offense Description"]));
+    }
+
+  }
+
+  // Add our marker cluster layer to the map
+  myMap.addLayer(markers);
+
 });
-  
-
-function createFeatures(inpData) {
-  // Sending our earthquakes layer to the createMap function
-  createMap(inpData);
-}
-
-function createMap(crimes) {
-
-  // Define streetmap and darkmap layers
-  var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.streets",
-    accessToken: API_KEY
-  });
-
-
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
-  var myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 5,
-    layers: [streetmap]
-  });
-
-  // Loop through the cities array and create one marker for each city, bind a popup containing its name and population add it to the map
-  for (var i = 0; i < 1000; i++) {
-    var city = crimes[i];
-    L.marker([city.Latitude, city.Longitude])
-      .bindPopup("<h1>" + city.City + "</h1> <hr> <h3>Crime " + city["Offense Description"] + "</h3>")
-      .addTo(myMap);
-  } 
-  
-  // Loop through each metro array and create a marker , bind a popup containing its name and population add it to the map
-//  for (var i = 0; i < 51; i++) {
-//    var metro = neighborhood[i];
-//    L.marker([city.latitude, city.longitude])
-//      .bindPopup("<h1>" + metro.Metro + "</h1> <hr> <h3>neighborhood " + metro["Average Price"] + "</h3>")
-//      .addTo(myMap);
-//  }   
-}
