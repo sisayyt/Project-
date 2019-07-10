2434a02daf7e573c43332e01045c7d24754cccde
Van86 <47127087+Van86@users.noreply.github.com>
2019-07-09 22:47:37 -0500

    Update zillowdata.js

--- website/static/js/zillowdata.js
+++ website/static/js/zillowdata.js
@@ -1,47 +1,73 @@
-// Create our map, giving it the streetmap and earthquakes layers to display on load
-var myMapZ = L.map("zillowmap", {
-  center: [
-    37.09, -95.71
-  ],
-  zoom: 5,
-  layers: [streetmapZ]
-});
-
 // Store our API endpoint inside queryUrl
-var queryUrl = "../static/data/zillow.json"
-console.log('Im running!')
-
+var queryUrl = "zillow.geojson"
+var queryUrl1 = "crime.geojson"
 // Perform a GET request to the query URL
-d3.json(queryUrl).then((data) =>{
-    // Once we get a response, send the data.features object to the createFeatures function
-  createFeaturesZ(data);
-  console.log('[Data]: ', data)
-  //alert(data)
+d3.json(queryUrl, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
 });
-  
 
-function createFeaturesZ(inpDataZ) {
+function createFeatures(HomeData) {
+
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.name +
+      "</h3><hr><p>" + (feature.properties["Home Value"]) + "</p>");
+  }
+
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var Neighborhood_Homes = L.geoJSON(HomeData, {
+    onEachFeature: onEachFeature
+  });
+
   // Sending our earthquakes layer to the createMap function
-  createMapZ(inpDataZ);
+  createMap(Neighborhood_Homes);
 }
 
-function createMapZ(crimesZ) {
+function createMap(Neighborhood_Homes) {
 
   // Define streetmap and darkmap layers
-  var streetmapZ = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "mapbox.streets",
     accessToken: API_KEY
   });
 
+  var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.dark",
+    accessToken: API_KEY
+  });
 
+  // Define a baseMaps object to hold our base layers
+  var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap
+  };
 
-  // Loop through the cities array and create one marker for each city, bind a popup containing its name and population add it to the map
-  for (var i = 0; i < 5; i++) {
-    var cityZ = crimesZ[i];
-    L.marker([cityZ.latitude, cityZ.longitude])
-      .bindPopup("<h1>" + cityZ.Metro + "</h1> <hr> <h3>Crime " + cityZ["Offense Description"] + "</h3>")
-      .addTo(myMapZ);
-  }  
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    Neighborhood_Homes: Neighborhood_Homes,
+    // Crime: Crime
+  };
+
+  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  var myMap = L.map("map", {
+    center: [
+      32.7763, -96.7969
+    ],
+    zoom: 10,
+    layers: [streetmap, Neighborhood_Homes]
+  });
+
+  // Create a layer control
+  // Pass in our baseMaps and overlayMaps
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
 }
